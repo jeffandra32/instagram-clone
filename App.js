58b9a747697826826f7add31c46e0323bedd80c9
jeffandra32 <jef_xtz@yahoo.com.br>
2020-09-25 18:52:21 -0300

    feat: Add Gallery e Search

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import {View, StyleSheet} from 'react-native';
 import SplashScreen from './src/screens/SplashScreen';
 import InitialLaunchScreen from './src/screens/InitialLaunchScreen';
 import FeedScreen from './src/screens/FeedScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import GalleryScreen from './src/screens/GalleryScreen';
 
 const App: () => React$Node = () => {
   return (
     <View style={styles.container}>
-      <FeedScreen />
+      <GalleryScreen />
     </View>
   );
 };
